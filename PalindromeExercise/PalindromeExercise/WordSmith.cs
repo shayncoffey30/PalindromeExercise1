cf01ba6c5448225e1535933ab0c446b1de2539d5
Shayn Coffey <Shayncoffey30@gmail.com>
2025-05-20 18:12:19 -0400

    finished

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string palin)
+        {
+            if (palin == null || palin.Length == 0)
+            {
+                return false;
+
+            }
+
+            //string wordLowered = word.ToLower();
+            //string backwards = "";
+
+            //for (int i = wordLowered.Length - 1; i >= 0; i--)
+            //{
+            //    backwards += wordLowered[i];
+            //}
+
+            //return wordLowered == backwards;
+
+            //if (wordLowered == backwards)
+            //{
+            //    return true;
+            //}
+            //else
+            //{
+            //    return false;
+            //}
+            return palin.ToLower().SequenceEqual(palin.ToLower().Reverse());
+
+
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,28 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("Racecar", true)]
+        [InlineData("hannah", true)]
+        [InlineData("kayak", true)]
+        [InlineData("John", false)]
+        [InlineData("Jose", false)]
+        [InlineData("Amoriss", false)]
+        [InlineData("", false)]
+        [InlineData(null, false)]
+        public void IsAPalindromeTest(string palin, bool expected)
         {
 
+            // Arrange
+            var ws = new WordSmith();
+
+            // Act
+            var actual = ws.IsAPalindrome(palin);
+
+            // Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
